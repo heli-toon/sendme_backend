17a77185a64274f45e6cce9857fc57999b698c4b
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2024-06-05 16:43:32 +0000

    Add User Models

--- sendme_backend/api/models.py
+++ sendme_backend/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class UserProfile(models.Model):
+    email = models.EmailField()
+    id_user = models.IntegerField()
+    phone = models.TextField(blank=True)
+    gender = models.TextField(blank=True)
+    full_name = models.TextField(blank=True)
+    bio = models.CharField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    rough_location = models.TextField(max_length=100, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='')
+    location_radius = models.TextField(blank=True)
+    verified = models.BooleanField(blank=True)
+    currency = models.TextField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+# AdPost
+# Announcements
+# Waitlist
--- sendme_backend/api/serializers.py
+++ sendme_backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import UserProfile
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,11 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['email', 'id_user', 'phone', 'gender', 'full_name', 'bio', 'rough_location', 'user', 'currency', 'created_at', 'verified']
+        extra_kwargs = {'user': {'read_only' : True}}
+
+    
